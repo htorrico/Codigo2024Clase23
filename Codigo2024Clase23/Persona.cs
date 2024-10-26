ef740c6b2cb51ba2575b874f9265e7038bac5f53
Hugo Torrico <torricomarquezh@gmail.com>
2024-10-25 20:34:50 -0500

    Binding  ListView

---
+++ Codigo2024Clase23/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codigo2024Clase23
+{
+    public class Persona
+    {
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+
+        public int Edad { get; set; }
+    }
+}
---
+++ Codigo2024Clase23/PersonasWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Codigo2024Clase23
+{
+    /// <summary>
+    /// Lógica de interacción para PersonasWindow.xaml
+    /// </summary>
+    public partial class PersonasWindow : Window
+    {
+        public PersonasWindow()
+        {
+            InitializeComponent();
+        }
+      
+
+        private void btnBuscar_Click(object sender, RoutedEventArgs e)
+        {
+
+            List<Persona> personas = new List<Persona>();
+            personas.Add(new Persona { Apellidos = "SEVILLA", Nombres = "IVAN", Edad = 36 });
+            personas.Add(new Persona { Apellidos = "JORGE", Nombres = "BRANDON", Edad = 25 });
+            personas.Add(new Persona { Apellidos = "INGA", Nombres = "JHOY", Edad = 25 });
+            personas.Add(new Persona { Apellidos = "CUEVAS", Nombres = "RONALD", Edad = 30 });
+            personas.Add(new Persona { Apellidos = "TORRICO", Nombres = "HUGO", Edad = 32 });
+
+
+            lvPersonas.ItemsSource = personas;
+
+        }
+    }
+}
